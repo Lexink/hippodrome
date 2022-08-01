800ac1be4ba42705a1301facec1e1d40a8eb785a
lexink <lexink@mail.ru>
2022-08-02 00:30:41 +0300

    Add check blank first parameter test for Horse constructor

--- src/test/java/HorseTest.java
+++ src/test/java/HorseTest.java
@@ -1,4 +1,6 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -10,5 +12,14 @@ public class HorseTest {
                 () -> new Horse(null, 0.0)
         );
         assertEquals("Name cannot be null.", exception.getMessage());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {" ", "\n", "\t"})
+    public void whenConstructorHasBlankFirstParameter(String parameter) {
+        Throwable exception = assertThrows(IllegalArgumentException.class,
+                () -> new Horse(parameter, 0.0)
+        );
+        assertEquals("Name cannot be blank.", exception.getMessage());
     }
 }
